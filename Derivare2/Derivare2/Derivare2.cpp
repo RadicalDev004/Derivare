9583f7dbc0e63cf32533f66a6a0b1f64268f2d5f
RadicalDev004 <bspilevoi@yahoo.com>
2023-12-05 14:09:32 +0200

    Added graphics

---
+++ Derivare2/Derivare2/Derivare2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "graphics.h"
+#pragma comment(lib,"graphics.lib")
+using namespace std;
+
+int main()
+{
+    initwindow(500, 500);
+    getch();
+    closegraph();
+    return 0;
+}
+
