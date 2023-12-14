e621ab0128793fc43b657a128d8b54f311a51170
RadicalDev004 <bspilevoi@yahoo.com>
2023-12-14 20:40:39 +0200

    optimizations #1

--- Derivare2/Derivare2/ArboreBInar.cpp
+++ Derivare2/Derivare2/ArboreBInar.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+using namespace std;
 
 class ArbNod {
 	
 public:
 	ArbNod *St, *Dr;
-	char info;
-	ArbNod(char inf)
+	string info;
+	ArbNod(string inf)
 	{
 		St = Dr = nullptr;
 		info = inf;
 	}
-	ArbNod(char inf, ArbNod *n, bool nod = 0)
+	ArbNod(string inf, ArbNod *n, bool nod = 0)
 	{
 		if(nod == 0)
 			St = n, Dr = nullptr;
@@ -19,11 +20,17 @@ public:
 
 		info = inf;
 	}
-	ArbNod(char inf, ArbNod* st, ArbNod *dr)
+	ArbNod(string inf, ArbNod* st, ArbNod *dr)
 	{
 		St = st;
 		Dr = dr;
 		info = inf;
+	}
+	ArbNod(ArbNod* copy)
+	{
+		St = copy->St;
+		Dr = copy->Dr;
+		info = copy->info;
 	}
 	bool HasChildren()
 	{
@@ -31,4 +38,9 @@ public:
 	}
 
 	static ArbNod *Temp;
+
+	bool operator == (ArbNod* other)
+	{
+		return other->St == this->St && other->Dr == this->Dr && other->info == this->info;
+	}
 };
--- Derivare2/Derivare2/Derivare2.cpp
+++ Derivare2/Derivare2/Derivare2.cpp
@@ -1,54 +1,128 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <stack>
 #include <queue>
 #include "graphics.h"
 #include "ArboreBinar.cpp"
 #pragma comment(lib,"graphics.lib")
+
 using namespace std;
+
+ifstream fin("fisier.in");
+ofstream fout("fisier.out");
+
 ArbNod* ArbNod::Temp = nullptr;
 
-// Verifica parantezele expresiei matematice
-bool verificaParanteze(std::string expresie) {
-    std::stack<char> stiva;
-    for (char ch : expresie) {
-        if (ch == '(') {
-            stiva.push(ch);
-        }
-        else if (ch == ')') {
-            if (stiva.empty()) return false;
-            stiva.pop();
-        }
+//operatii pe arbori/expresii
+bool verificaParanteze(string expresie);
+bool verificare(string expresie);
+int prioritate(char operatorr);
+string postfixare(string expresie);
+
+//Derivare propriu-zisa
+void Derivare(ArbNod* oper);
+string DerivareIndiv(string info);
+
+//Parcurgere si procesare
+void SDR(ArbNod* rad);
+void SRD(ArbNod* rad);
+
+int main()
+{
+    string expresie;
+    fin >> expresie;
+
+    if (verificare(expresie)) {
+        string arborePostfixat = postfixare(expresie);
+
+        SRD(ArbNod::Temp);
+        
+        SDR(ArbNod::Temp);
+        fout << '\n';
+        SRD(ArbNod::Temp);
     }
-    return stiva.empty();  // daca stiva este goala, parantezele sunt corecte
-}
-
-// Verifica validitatea expresiei matematice
-bool verificare(std::string expresie) {
-    return verificaParanteze(expresie);
-    // adauga alte verificari daca este necesar
-}
-
-// Returneaza prioritatea operatorilor
-int prioritate(char operatorr) {
-    switch (operatorr) {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    case '^':
-        return 3;
-    default:
-        return 0;
+    else {
+        fout << "Expresia matematica introdusa este invalida." << std::endl;
     }
+
+    return 0;
 }
 
-// Converteste expresia intr-un arbore postfixat
-std::string postfixare(std::string expresie) {
-    std::stack<char> stiva;
-    std::string arborePostfixat;
+string DerivareIndiv(string info)
+{
+    if (info == "+" || info == "^" || info == "*") return info;
+
+    if (isalpha(info[0]))
+        return "1";
+    if (isdigit(info[0]))
+        return "0";
+
+    return "?";
+}
+void Derivare(ArbNod* oper)
+{
+    if (oper->info == "+")
+    {
+        oper->St->info = DerivareIndiv(oper->St->info);
+        oper->Dr->info = DerivareIndiv(oper->Dr->info);
+    }
+    if (oper->info == "^")
+    {
+        //daca fiul stanga e variabila si fiul dreapta e nr avem x^c unde c e contanta (ex c = 5)
+        if (isalpha(oper->St->info[0]) && isdigit(oper->Dr->info[0])) {
+            int a = stoi((oper->Dr->info));
+            a--;
+            oper->Dr->info = to_string(stoi((oper->Dr->info)) - 1);
+
+            // realizam un nou nod deoarece x^5 devine 5*x^4, din desen 1 trebuie sa devina desen 2
+            /*      
+            *      ^
+            *     / \
+            *    x   5
+            */
+            /*
+            *       * (- oper)
+            *      / \
+            *     /   \
+            *    5     ^ (- copy)
+            *         / \
+            *        x   4
+            */
+
+            ArbNod* copy = new ArbNod(oper);
+            oper->Dr = copy;
+            oper->St = new ArbNod(copy->Dr);
+            oper->St->info = to_string(stoi((copy->Dr->info)) + 1);
+            oper->info = '*';
+        }              
+        
+    }
+
+}
+void SRD(ArbNod* rad)
+{
+    //parcurgere inordine + afisare
+    if (rad->St != nullptr)
+        SRD(rad->St);
+    fout << rad->info;
+    if (rad->Dr != nullptr)
+        SRD(rad->Dr);
+}
+void SDR(ArbNod* rad)
+{
+    //parcurgere postordine + derivare
+    if (rad->St != nullptr)
+        SDR(rad->St);
+    if (rad->Dr != nullptr)
+        SDR(rad->Dr);
+
+    if (rad->HasChildren())
+        Derivare(rad);
+}
+string postfixare(string expresie) {
+    stack<char> stiva;
+    string arborePostfixat;
     for (char ch : expresie) {
         if (isdigit(ch) || isalpha(ch)) {  // daca este numar sau litera
             arborePostfixat += ch;
@@ -66,27 +140,40 @@ std::string postfixare(std::string expresie) {
         else {  // daca este operator
             while (!stiva.empty() && prioritate(ch) <= prioritate(stiva.top())) {
 
-                if (arborePostfixat.size() != 1) {
-                    ArbNod *newNod = new ArbNod(
-                        stiva.top(), 
-                        new ArbNod(arborePostfixat.at(arborePostfixat.size() - 2)), 
-                        new ArbNod(arborePostfixat.at(arborePostfixat.size() - 1))
-                        );
-                   
+                //daca in stringul arborelui sunt mai mult de 2 elemente inseamna ca trebuie un nou varf cu 2 fii (o operatie noua)
+                if (arborePostfixat.size() > 1) {
+                    ArbNod* newNod = new ArbNod(
+                        string(1, stiva.top()),
+                        new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 2))),
+                        new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 1)))
+                    );
+
                     arborePostfixat.pop_back(); arborePostfixat.pop_back();
-                    ArbNod::Temp = newNod;
+
+                    // Daca operatia anterioara nu e null inseamna ca trebuie sa unim 2 operatii, cea curenta si cea anterioara cu semnul de la stiva.top()
+                    if (ArbNod::Temp != nullptr) {
+                        stiva.pop();
+                        ArbNod* tp = new ArbNod(
+                            string(1, stiva.top()),
+                            newNod,
+                            ArbNod::Temp
+                        );
+                        ArbNod::Temp = tp;
+                    }
+                    else
+                        ArbNod::Temp = newNod;
                 }
-                else {
-                    ArbNod *newNod = new ArbNod(
-                        stiva.top(), 
-                        new ArbNod(arborePostfixat.at(arborePostfixat.size() - 1)),
+                //altfel inseamna ca a ramas un singur element care trebuie pus in operatie cu operatia anterioara
+                else { 
+                    ArbNod* newNod = new ArbNod(
+                        string(1, stiva.top()),
+                        new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 1))),
                         ArbNod::Temp
                     );
 
                     arborePostfixat.pop_back();
                     ArbNod::Temp = newNod;
                 }
-                
 
                 //arborePostfixat += stiva.top();
                 stiva.pop();
@@ -94,12 +181,13 @@ std::string postfixare(std::string expresie) {
             stiva.push(ch);
         }
     }
+    // se aplica regulile de construire de la while anterior doar ca aparent am scris invers cele 2 conditii
     while (!stiva.empty()) {
 
         if (arborePostfixat.size() < 2) {
             ArbNod* newNod = new ArbNod(
-                stiva.top(),
-                new ArbNod(arborePostfixat.at(arborePostfixat.size() - 1)),
+                string(1, stiva.top()),
+                new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 1))),
                 ArbNod::Temp
             );
 
@@ -108,17 +196,17 @@ std::string postfixare(std::string expresie) {
         }
         else {
             ArbNod* newNod = new ArbNod(
-                stiva.top(),
-                new ArbNod(arborePostfixat.at(arborePostfixat.size() - 2)),
-                new ArbNod(arborePostfixat.at(arborePostfixat.size() - 1))
+                string(1, stiva.top()),
+                new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 2))),
+                new ArbNod(string(1, arborePostfixat.at(arborePostfixat.size() - 1)))
             );
-            
+
             arborePostfixat.pop_back(); arborePostfixat.pop_back();
 
             if (ArbNod::Temp != nullptr) {
                 stiva.pop();
                 ArbNod* tp = new ArbNod(
-                    stiva.top(),
+                    string(1, stiva.top()),
                     newNod,
                     ArbNod::Temp
                 );
@@ -126,84 +214,41 @@ std::string postfixare(std::string expresie) {
             }
             else
                 ArbNod::Temp = newNod;
-        }      
+        }
 
         //arborePostfixat += stiva.top();
         stiva.pop();
     }
     return arborePostfixat;
 }
-
-void Derivare(ArbNod* oper);
-char DerivareIndiv(char info);
-void SDR(ArbNod* rad);
-void SRD(ArbNod* rad);
-
-int main()
-{
-    std::string expresie;
-    std::cout << "Introduceti o expresie matematica: ";
-    std::cin >> expresie;
-
-    if (verificare(expresie)) {
-        std::string arborePostfixat = postfixare(expresie);
-
-        SRD(ArbNod::Temp);
-        
-        SDR(ArbNod::Temp);
-        cout << '\n';
-        SRD(ArbNod::Temp);
-    }
-    else {
-        std::cout << "Expresia matematica introdusa este invalida." << std::endl;
-    }
-
-    return 0;
-}
-
-char DerivareIndiv(char info)
-{
-    if (isalpha(info))
-        return '1';
-    if (isdigit(info))
-        return '0';
-
-}
-void Derivare(ArbNod* oper)
-{
-    switch (oper->info)
-    {
+int prioritate(char operatorr) {
+    switch (operatorr) {
     case '+':
-    {
-        oper->St->info = DerivareIndiv(oper->St->info);
-        oper->Dr->info = DerivareIndiv(oper->Dr->info);
-        break;
-    }
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
     case '^':
-    {
-        if (isalpha(oper->St->info) && isdigit(oper->Dr->info)) {
-            oper->Dr->info = (oper->Dr->info - '0' - 1 + '0');
+        return 3;
+    default:
+        return 0;
+    }
+}
+bool verificaParanteze(std::string expresie) {
+    std::stack<char> stiva;
+    for (char ch : expresie) {
+        if (ch == '(') {
+            stiva.push(ch);
         }
-        break;
+        else if (ch == ')') {
+            if (stiva.empty()) return false;
+            stiva.pop();
+        }
     }
-    }
-
+    return stiva.empty();  // daca stiva este goala, parantezele sunt corecte
 }
-void SRD(ArbNod* rad)
-{
-    if (rad->St != nullptr)
-        SRD(rad->St);
-    cout << rad->info;
-    if (rad->Dr != nullptr)
-        SRD(rad->Dr);
-}
-void SDR(ArbNod* rad)
-{
-    if (rad->St != nullptr)
-        SDR(rad->St);
-    if (rad->Dr != nullptr)
-        SDR(rad->Dr);
-
-    if (rad->HasChildren() && !rad->St->HasChildren() && !rad->Dr->HasChildren())
-        Derivare(rad);
+bool verificare(string expresie) {
+    return verificaParanteze(expresie);
+    // adauga alte verificari daca este necesar
 }
